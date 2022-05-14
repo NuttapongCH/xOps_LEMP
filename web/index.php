ca0e7585ceb72cbd32bbaf31eac1f9922df7894d
Nuttapong Chiangdoem <104500957+NuttapongCH@users.noreply.github.com>
2022-05-14 14:20:15 +0700

    commit on 'main'

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $servername = "db";
+ $username = "root";
+ $password = "sev1234";
+ 
+ // Create connection
+ $conn = new mysqli($servername, $username, $password);
+ 
+ // Check connection
+ if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+ }
+ echo "Connected successfully";
+
+
+?>
